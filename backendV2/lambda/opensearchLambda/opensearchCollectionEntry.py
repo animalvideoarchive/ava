54f18353bec4b79d9de92c61a39fedbe9672a244
nikhilka27 <nagraw20@asu.edu>
2024-10-07 19:23:14 -0700

    added cdk code for lambda func

---
+++ backendV2/lambda/opensearchLambda/opensearchCollectionEntry.py
@@ -0,0 +1,77 @@
+import os
+import json
+import boto3
+import uuid
+from datetime import datetime
+from opensearchpy import OpenSearch, RequestsHttpConnection, AWSV4SignerAuth
+
+# Get the OpenSearch domain name from environment variable
+OPENSEARCH_COLLECTION_ENDPOINT = os.getenv('COLLECTION_ENDPOINT')
+INDEX_NAME = os.getenv('INDEX_NAME')
+
+# default lambda region
+region = os.getenv('AWS_REGION')
+
+# Initialize AWS clients
+service = 'aoss'
+credentials = boto3.Session().get_credentials()
+auth = AWSV4SignerAuth(credentials, region, service)
+
+# create an opensearch client and use the request-signer
+client = OpenSearch(
+    hosts=[{'host': host, 'port': 443}],
+    http_auth=auth,
+    use_ssl=True,
+    verify_certs=True,
+    connection_class=RequestsHttpConnection,
+    timeout=30 
+)
+
+def generate_unique_id():
+    """Generate a unique ID using timestamp and UUID"""
+    timestamp = datetime.utcnow().strftime('%Y%m%d%H%M%S%f')
+    unique_id = f"{timestamp}-{str(uuid.uuid4())}"
+    return unique_id
+
+def lambda_handler(event, context):
+    try:
+        # Extract the video metadata from the event
+        video_metadata = event['video_metadata']
+        
+        # Generate a unique document ID
+        doc_id = generate_unique_id()
+        
+        # Prepare the document for indexing
+        document = {
+            "id": doc_id,
+            **video_metadata
+        }
+        
+        response = client.index(
+            index = INDEX_NAME,
+            body = document,
+            id = doc_id
+        )
+
+        if response['result'] == 'created':
+            print(f"Document indexed successfully: {doc_id}")
+            return {
+                'statusCode': 200,
+                'body': json.dumps({
+                    'message': 'Document successfully indexed in OpenSearch',
+                    'document_id': doc_id
+                })
+            }
+        else:
+            print(f"Document indexing failed: {response}")
+            return {
+                'statusCode': response['ResponseMetadata']['HTTPStatusCode'],
+                'body': json.dumps(f'Error indexing document: {response}')
+            }
+    
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return {
+            'statusCode': 500,
+            'body': json.dumps(f'Error: {str(e)}')
+        }
--- backendV2/lib/multipart_s3_upload-stack.ts
+++ backendV2/lib/multipart_s3_upload-stack.ts
@@ -6,19 +6,31 @@ import { BucketEncryption } from 'aws-cdk-lib/aws-s3';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { join } from 'path';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
+import * as ops from 'aws-cdk-lib/aws-opensearchserverless';
+import * as url from 'url';
 
 export class MultipartS3UploadStack extends cdk.Stack {
+  public readonly searchDomain: string;
+  
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     const env = cdk.Stack.of(this).node.tryGetContext('env');
-    const expires = cdk.Stack.of(this).node.tryGetContext('urlExpiry') ?? '300';
-    const timeout = Number(cdk.Stack.of(this).node.tryGetContext('functionTimeout') ?? '3');
+    const expires = cdk.Stack.of(this).node.tryGetContext('urlExpiry') ?? '43200';
+    const timeout = Number(cdk.Stack.of(this).node.tryGetContext('functionTimeout') ?? '60');
 
-    const s3Bucket = new s3.Bucket(this, "document-upload-bucket", {
+    const s3Bucket = new s3.Bucket(this, "document-upload-bucket-new", {
       bucketName: `document-client-upload-${env}`,
       lifecycleRules: [{
         expiration: cdk.Duration.days(10),
         abortIncompleteMultipartUploadAfter: cdk.Duration.days(1),
+        // transitions: [{
+        //   storageClass: s3.StorageClass.DEEP_ARCHIVE,
+        //   transitionAfter: cdk.Duration.days(1), // Transition to Glacier after 1 day
+        // }],    
       }],
       blockPublicAccess: {
         blockPublicAcls: true,
@@ -42,6 +54,7 @@ export class MultipartS3UploadStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY
     });
 
+    // need to check this whitelisting
     const whitelistedIps = [cdk.Stack.of(this).node.tryGetContext('whitelistip')]
 
     const apiResourcePolicy = new iam.PolicyDocument({
@@ -92,6 +105,7 @@ export class MultipartS3UploadStack extends cdk.Stack {
       },
       functionName: `multipart-upload-initialize-${env}`
     });
+
     const getPreSignedUrlsLambda = new NodejsFunction(this, 'getPreSignedUrlsHandler', {
       ...commonNodeJsProps,
       entry: join(__dirname, '../lambda/getPreSignedUrls.js'),
@@ -142,6 +156,201 @@ export class MultipartS3UploadStack extends cdk.Stack {
         rateLimit: 100,
         burstLimit: 200
       },
+    });
+    
+
+    const s3OutBucket = new s3.Bucket(this, "document-clipping-thumbnail-bucket", {
+      bucketName: `document-ecs-clipping-thumbnail-upload-${env}`,
+      lifecycleRules: [{
+        expiration: cdk.Duration.days(10),
+        abortIncompleteMultipartUploadAfter: cdk.Duration.days(1),
+      }],
+      blockPublicAccess: {
+        blockPublicAcls: true,
+        blockPublicPolicy: true,
+        ignorePublicAcls: true,
+        restrictPublicBuckets: true,
+      },
+      encryption: BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+      cors: [{
+        allowedOrigins: ["*"],
+        allowedHeaders: ["*"],
+        allowedMethods: [
+          s3.HttpMethods.GET,
+          s3.HttpMethods.PUT,
+          s3.HttpMethods.POST,
+        ],
+      }],
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+
+    // Create a VPC for the ECS Cluster
+    // const vpc = new ec2.Vpc(this, 'Vpc', {
+    //   maxAzs: 2 // Default is all AZs in the region
+    // });
+    const vpc = new ec2.Vpc(this, 'MyVpc', {
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'Public',
+          subnetType: ec2.SubnetType.PUBLIC,
+        }
+      ],
+      natGateways: 0
+    });
+    
+    // TODO: Need to add cluster in private subnet
+    // Create an ECS Cluster
+    const cluster = new ecs.Cluster(this, 'EcsCluster', {
+      vpc,
+    });
+
+    const taskRole = new iam.Role(this, 'EcsTaskRole', {
+      assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com')
+    });
+
+    s3Bucket.grantReadWrite(taskRole);
+    s3OutBucket.grantReadWrite(taskRole);
+
+    // Define Fargate Task Definition
+    const taskDefinition = new ecs.FargateTaskDefinition(this, 'TaskDef', {
+      memoryLimitMiB: 512,
+      cpu: 256,
+      taskRole: taskRole // Assign the task role to the Fargate task
+    });
+
+    // Add container to the task definition
+    const container = taskDefinition.addContainer('MyContainer', {
+      image: ecs.ContainerImage.fromRegistry('nikhildocker7/ffmpegprocesing:latest'),
+      logging: new ecs.AwsLogDriver({
+        streamPrefix: 'ecs'
+      }),
+    });
+
+    // Expose a port (optional)
+    container.addPortMappings({
+      containerPort: 80,
+    });
+
+    const commonNodeJSLambdaProps = {
+      bundling: {
+        externalModules: [
+          '@aws-sdk'        ],
+      },
+      runtime: Runtime.NODEJS_18_X,
+    };
+    
+    // Create a Lambda function to trigger the ECS task
+    const lambdaFunction = new NodejsFunction(this, 'EcsTriggerLambda', {
+      ...commonNodeJSLambdaProps,
+      entry: join(__dirname, '../lambda/ecsTrigger.js'),
+      environment: {
+        CLUSTER_NAME: cluster.clusterName,
+        TASK_DEFINITION_ARN: taskDefinition.taskDefinitionArn,
+        // VPC_SUBNETS: JSON.stringify(vpc.selectSubnets({ subnetType: ec2.SubnetType.PUBLIC }).subnetIds),
+        VPC_SUBNETS: JSON.stringify(vpc.selectSubnets({ subnetType: ec2.SubnetType.PUBLIC }).subnetIds),
+        S3_OUT_BUCKET: s3OutBucket.bucketName,
+      },
+      functionName: `ecs-trigger-${env}`,
+      timeout: cdk.Duration.seconds(timeout),
+    });
+
+    // Grant Lambda permissions to run ECS tasks
+    lambdaFunction.addToRolePolicy(new iam.PolicyStatement({
+      actions: ['ecs:RunTask', 'ecs:DescribeTasks', 'iam:PassRole'],
+      resources: [taskDefinition.taskDefinitionArn, taskRole.roleArn],
+    }));
+    
+    // // Grant Lambda permissions to pass task role
+    lambdaFunction.addToRolePolicy(new iam.PolicyStatement({
+      actions: ['iam:PassRole'],
+      resources: ['*'],
+      conditions: {
+        StringLike: {
+          'iam:PassedToService': 'ecs-tasks.amazonaws.com'
+        }
+      }
+    }));
+
+    // Add an S3 event notification to trigger Lambda on object creation
+    s3Bucket.addEventNotification(s3.EventType.OBJECT_CREATED, new s3n.LambdaDestination(lambdaFunction)); 
+
+    // Define the Lambda function
+    const myClaudeLambda = new lambda.Function(this, 'MyClaudeLambda', {
+      runtime: Runtime.PYTHON_3_10,  // Runtime version
+      code: lambda.Code.fromAsset('lambda'),  // directory containing 'app.py' and any other dependencies
+      handler: 'getMetadataFromBedrockClaude.lambda_handler',  // file is 'app.py' and function is 'handler'
+      functionName: `bedrock-claude-call-${env}`,
+    });
+
+    myClaudeLambda.addToRolePolicy(new iam.PolicyStatement({
+      actions: ["bedrock:*"],  // Adjust based on the specific Bedrock actions required
+      resources: ["*"],
+    }));
+
+    s3OutBucket.grantReadWrite(myClaudeLambda);
+
+
+    // Create an OpenSearch Service domain
+    // See https://docs.aws.amazon.com/opensearch-service/latest/developerguide/serverless-manage.html
+    const collection = new ops.CfnCollection(this, 'ProductSearchCollection', {
+      name: 'product-collection',
+      type: 'SEARCH',
+      description: "This collections will store the metadata of admin upload vidoes for zoo ",
+    });
+
+    // Encryption policy is needed in order for the collection to be created
+    const encPolicy = new ops.CfnSecurityPolicy(this, 'ProductSecurityPolicy', {
+      name: 'product-collection-policy',
+      policy: '{"Rules":[{"ResourceType":"collection","Resource":["collection/product-collection"]}],"AWSOwnedKey":true}',
+      type: 'encryption'
+    });
+    collection.addDependency(encPolicy);
+
+    // Network policy is required so that the dashboard can be viewed!
+    const netPolicy = new ops.CfnSecurityPolicy(this, 'ProductNetworkPolicy', {
+      name: 'product-network-policy',
+      policy: '[{"Rules":[{"ResourceType":"collection","Resource":["collection/product-collection"]}, {"ResourceType":"dashboard","Resource":["collection/product-collection"]}],"AllowFromPublic":true}]',
+      type: 'network'
+    });
+    collection.addDependency(netPolicy);
+
+    const parsedDomainUrl = new url.URL(collection.attrCollectionEndpoint);
+    const domainHostname = parsedDomainUrl.hostname;
+    
+    new cdk.CfnOutput(this, 'DashboardEndpoint', {
+      value: collection.attrDashboardEndpoint,
+    });
+
+    new cdk.CfnOutput(this, 'DashboardHost', {
+      value: domainHostname,
+    });
+
+    // Define the Lambda function
+    const myOpenSearchEntryCreationLambda = new lambda.Function(this, 'MyOpenSearchEntryCreationLambda', {
+      runtime: Runtime.PYTHON_3_10,  // Runtime version
+      handler: 'opensearchCollectionEntry.lambda_handler',  // file is 'app.py' and function is 'handler'
+      functionName: `opensearch-entry-creation-${env}`,
+      code: lambda.Code.fromDockerBuild("lambda/opensearchLambda"),
+      environment: {
+        COLLECTION_ENDPOINT : domainHostname,
+        INDEX_NAME : 'product-collection-index',
+      },
+      timeout: cdk.Duration.minutes(2),
+    });
+    
+    myOpenSearchEntryCreationLambda.addToRolePolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: ['aoss:*'],
+      resources: ['*']
+    }));
+
+    new ops.CfnAccessPolicy(this, 'ProductDataPolicy', {
+      name: 'product-data-policy',
+      policy: `[{"Description": "Access from lambda role to push", "Rules":[{"ResourceType":"index","Resource":["index/product-collection/*"],"Permission":["aoss:*"]}, {"ResourceType":"collection","Resource":["collection/product-collection"],"Permission":["aoss:*"]}], "Principal":["${myOpenSearchEntryCreationLambda.role?.roleArn}"]}]`,
+      type: 'data'
     });
   }
 }
